c5866c92dea33e236f18653a44942be6738152d8
Astridcc01 <93380210+Astridcc01@users.noreply.github.com>
2021-12-10 12:56:06 +0100

    Create Notas.java

---
+++ Notas.java
@@ -0,0 +1,23 @@
+package com.primero;
+
+public class Notas {
+	//atributo
+	public float nota;
+	
+	//constructor
+	public Notas(float nota) {
+		super();
+		this.nota = nota;
+	}
+
+	//getter y setters
+		public float getNota() {
+		return nota;
+	}
+
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	
+	//genérico
+}
